179cbf1e7e52b1ec3a7f879b904a7f069aec19ef
Ryan Dempsey <ryandempsey@ryans-mbp.attlocal.net>
2019-07-12 13:43:11 -0500

    WIP - initial refactoring - move hotel list into own component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.style.scss'
 
 import hotelResultService from '../../services/hotel-result/hotel-result.service';
+import HotelList from '../hotel-list/hotel-list.component';
 
 const App = () => {
     const [hotels, setHotels] = useState([]);
@@ -29,34 +30,7 @@ const App = () => {
                     </div>
                 </div>
 
-                <div className="hotel-list">
-                    {hotels.map(hotel => (
-                        <div className="hotel-card" key={hotel.id}>
-                            <div
-                                className="image"
-                                style={{ backgroundImage: `url(${hotel.hotelStaticContent.mainImage.url})`}}>
-                            </div>
-                            <div className="hotel-details">
-                                <div className="hotel-name">
-                                    {hotel.hotelStaticContent.name}
-                                </div>
-                                <div className="location">
-                                    {hotel.hotelStaticContent.neighborhoodName}
-                                </div>
-                            </div>
-                            <div className="price-details">
-                                <span className="price">
-                                    <span dangerouslySetInnerHTML={{ __html: hotel.lowestAveragePrice.symbol }}></span>
-                                    {hotel.lowestAveragePrice.amount}
-                                </span>
-                                <span className="rewards">
-                                    {hotel.rewards.miles} miles
-                                </span>
-                                <button className="button">Select</button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                <HotelList hotels={hotels}/>
             </div>
         </div>
     )
