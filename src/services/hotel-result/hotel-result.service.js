9678340421ac643d0394ff347e1f5b6b69b66447
Ryan Dempsey <ryandempsey@ryans-mbp.attlocal.net>
2019-07-12 13:26:57 -0500

    WIP - initial error handling

--- src/services/hotel-result/hotel-result.service.js
+++ src/services/hotel-result/hotel-result.service.js
@@ -5,7 +5,15 @@ class HotelResultService {
         return axios
             .get('https://homework-app.rocketmiles.com/fe-homework/rates')
             .then(response => response.data)
-            .catch(() => {})
+            .catch((error) => {
+                if (error.response) {
+                    console.log('error.response.data.message', error.response.data.message);
+                } else if (error.request) {
+                    console.log('error.request', error.request);
+                } else {
+                    console.error('Error', error.message);
+                }
+            })
     }
 }
 
