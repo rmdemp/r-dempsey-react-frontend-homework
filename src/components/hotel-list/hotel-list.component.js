1de64c5082493a7f15796e3dc610902507ee57f6
Ryan Dempsey <ryandempsey@ryans-mbp.attlocal.net>
2019-07-13 17:41:04 -0500

    WIP - add functionality to pull in list filter criteria from parent component (App.js) via HotelFilter component

--- src/components/hotel-list/hotel-list.component.js
+++ src/components/hotel-list/hotel-list.component.js
@@ -3,17 +3,41 @@ import React from 'react';
 import HotelCard from '../hotel-card/hotel-card.component';
 
 const HotelList = props => {
+  if (props.filterBy) {
+    let searchTerm = new RegExp(props.filterBy, 'gi');
+    let filteredHotels = [];
+
+    props.hotels.forEach(hotel => {
+      if (searchTerm.test(hotel.hotelStaticContent.name)) {
+        filteredHotels.push(hotel);
+      }
+    })
+
+    return (
+      <div className="hotel-list">
+        {
+          filteredHotels.map(hotel => {
+            return (
+              <HotelCard hotel={hotel} />
+            )
+          })
+        }
+      </div>
+    )
+  }
+
   const hotels = props.hotels.map(hotel => {
       return (
         <HotelCard hotel={hotel} />
       )
     });
   
-  return (
-    <div className="hotel-list">
-      { hotels }
-    </div>
-  )
+    return (
+      <div className="hotel-list">
+        { hotels }
+      </div>
+    )
+
 }
 
 export default HotelList;
