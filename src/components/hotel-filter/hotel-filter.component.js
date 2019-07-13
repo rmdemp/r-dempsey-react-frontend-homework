916533853f830acaed47e7a9ab04856f48af87d8
Ryan Dempsey <ryandempsey@ryans-mbp.attlocal.net>
2019-07-13 12:47:27 -0500

    WIP - get and set value from select options

--- src/components/hotel-filter/hotel-filter.component.js
+++ src/components/hotel-filter/hotel-filter.component.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const HotelFilter = props => {
+  const [value, setValue] = useState('');
+
   return (
     <div className="filters">
         Hotel name
         <input type="text" className="input" maxLength={1}/>
         Price
-        <select name="" className="select">
-            <option value="">Recommended</option>
-            <option value="">Price low-to-high</option>
-            <option value="">Price high-to-low</option>
+        <select name="" className="select" value={value} onChange={(event) => setValue(event.target.value)}>
+            <option value="recommended">Recommended</option>
+            <option value="low">Price low-to-high</option>
+            <option value="high">Price high-to-low</option>
         </select>
         <button className="button">Reset</button>
     </div>
