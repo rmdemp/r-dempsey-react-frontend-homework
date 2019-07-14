6a1387188d5b1f206e4f14f6596f29b16b251fad
Ryan Dempsey <ryandempsey@ryans-mbp.attlocal.net>
2019-07-14 10:50:52 -0500

    WIP - initial functionality for reset hotel filters

--- src/components/hotel-filter/hotel-filter.component.js
+++ src/components/hotel-filter/hotel-filter.component.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const HotelFilter = props => {
   const [sortPriceBy, setSortPriceBy] = useState('');
   const [filterBy, setFilterBy] = useState('');
+  const inputRef = useRef(null);
 
   useEffect(() => {
     props.onPriceSortByChange(sortPriceBy);
@@ -17,17 +18,23 @@ const HotelFilter = props => {
     setFilterBy(e.target.value);
   }
 
+  const handleReset = () => {
+    inputRef.current.value = '';
+    setFilterBy('');
+    setSortPriceBy('');
+  }
+
   return (
     <div className="filters">
         Hotel name
-        <input type="text" className="input" onChange={(event) => handleNameFilterChange(event)}/>
+        <input ref={inputRef} type="text" className="input" onChange={(event) => handleNameFilterChange(event)}/>
         Price
         <select name="" className="select" onChange={(event) => handlePriceSortChange(event)}>
             <option value="recommended">Recommended</option>
             <option value="ascending">Price low-to-high</option>
             <option value="descending">Price high-to-low</option>
         </select>
-        <button className="button" onClick={(event) => handleReset(event)}>Reset</button>
+        <button className="button" onClick={() => handleReset()}>Reset</button>
     </div>
   );
 }
